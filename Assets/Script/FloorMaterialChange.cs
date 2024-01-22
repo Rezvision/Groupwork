adeb0f211931501d165efcd1654acf8c715e7bdb
Rezvision <147525543+Rezvision@users.noreply.github.com>
2024-01-22 23:43:26 +0000

    added animals to forest scene, added lighting details to hub, added fire to forest scene, added script to change floor material to burnt after time delay

---
+++ Assets/Script/FloorMaterialChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class FloorMaterialChanger : MonoBehaviour
+{
+    public Material newMaterial; // Drag and drop the new material in the Unity Editor
+
+    void Start()
+    {
+        StartCoroutine(ChangeMaterialAfterDelay());
+    }
+
+    IEnumerator ChangeMaterialAfterDelay()
+    {
+        yield return new WaitForSeconds(30f); // Wait for 30 seconds
+
+        Renderer floorRenderer = GetComponent<Renderer>();
+
+        if (floorRenderer != null)
+        {
+            floorRenderer.material = newMaterial; // Change the material
+        }
+        else
+        {
+            Debug.LogError("Renderer component not found on the floor object.");
+        }
+    }
+}
--- Assets/Script/Hub.cs
+++ Assets/Script/Hub.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.Mathematics;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class Hub : MonoBehaviour
 {
+    public Button ForestButton; // Reference to the forest button.
+    public Vector3 forestPosition = new Vector3(3f, 1f, -7f);
+    public Button urbanButton;
+    public Button antarticButton;
+    public InputActionReference forestTrigger;
+    public InputActionReference urbanTrigger;
+    public InputActionReference antarticTrigger;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        ForestButton.onClick.AddListener(Forest);
     }
 
     // Update is called once per frame
@@ -15,4 +29,10 @@ public class Hub : MonoBehaviour
     {
         
     }
-}
+    public void Forest()
+    {
+        // Display the game over UI and appropriate message.
+        player.transform.position = forestPosition; //player moved to target position /*transform.GetPositionAndRotation(out targetPosition,out Quaternion.ide);*/
+
+    }
+}   
