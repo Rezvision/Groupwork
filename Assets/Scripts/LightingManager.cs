4422faff2a11fc96c8ba5782d3da837ca80d6ef5
FSE_VR23 <FSE_VR23@LAPTOP-OE4HKTPU>
2024-01-24 14:19:09 +0000

    Added smaller asset folders containing assets used to create terrain

---
+++ Assets/Scripts/LightingManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24; //Clamp between 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }    
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+
+        RenderSettings.ambientLight = Preset.AmbientColour.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColour.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColour.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        if(RenderSettings.sun!=null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                        return;
+                }
+            }
+        }
+    }
+}
