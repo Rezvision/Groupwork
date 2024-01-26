444ddbcc9289f974f48ffe982ea42a75345f2d31
UniEthanWD <147525573+UniEthanWD@users.noreply.github.com>
2024-01-26 05:18:18 +0000

    quality of life movement speed and added scene changer to city 2
    needed to add a scene changer to hub after combined project

--- Assets/Ethan_Walton_Deaves/assets/scripts/changeSceen.cs
+++ Assets/Ethan_Walton_Deaves/assets/scripts/changeSceen.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class changeSceen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string scenename;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        if (other.CompareTag("Player")){
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            SceneManager.LoadScene(scenename);
+
+        }
     }
 }
